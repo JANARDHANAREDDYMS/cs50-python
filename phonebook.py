f9732008e1d57bd33638cadf3119daa0445a959a
Janardhana Reddy M S <76699907+JANARDHANAREDDYMS@users.noreply.github.com>
2022-03-24 17:55:47 +0530

    Add files via upload

---
+++ phonebook.py
@@ -0,0 +1,11 @@
+
+
+
+people = {
+    "Brian": "7892356541",
+    "David": "9880942645"
+    }
+
+    x=(input("Name:"))
+    if x in people:
+        print(f"Number: {people[x]}")
---
+++ speech.py
@@ -0,0 +1,8 @@
+ 
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("hello,world")
+engine.runAndWait()
+
+# run this in command prompt and computer tells out hello,world in audio
